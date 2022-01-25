e0c02645a4400ad874d23e366b28e0f803f4906a
abi <abilalu1994@gmail.com>
2022-01-25 12:28:58 -0600

    adding files to my repository

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # CodeQuiz
+## Mockup
+The following image shows the web application's appearance and functionality:
+![When you load the page you will see like this](sc1.png)
+![When you start the quiz, you will see this](sc2.png)
+![When you finish the quiz,you will see the results like this](sc3.png)
+
+When you load the app, you are asked to enter your name and the press start quiz button.
+The quiz have 5 multiple choice question with each question have 10 seconds of time to answer.
+When you click an answer, the right answer will turn green and if the option you clicked is wrong, it will turn red
+When you enter a wrong answer the timer stops and you will get only 10 seconds for the next question.
+When the timer runs out you are asked to go to next question by clicking next question button.
+Next qusetion button helps you to go to next question.
+At the end after finishing 5 questions you will be displayed the result with your name and asks you whether to replay or quit the quiz.
+
---
+++ js/questions.js
@@ -0,0 +1,59 @@
+// array foe questions and options
+let questions = [
+    {
+    numb: 1,
+    question: "In CSS, Select the property to set an image in a list instead of a standard bullet?",
+    answer: "list-style-image:",
+    options: [
+      "list-image:",
+      "list-style-image:",
+      "list-image-src:",
+      "image-list"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Select the correct option to create an e-mail link?",
+    answer: "A href=mailto:xxx@yyy.com",
+    options: [
+      "Mail href=xxx@yyy.com",
+      "Mail>xxx@yyy.com/mail",
+      "href=asfdsdgff",
+      "A href=mailto:xxx@yyy.com"
+    ]
+  },
+    {
+    numb: 3,
+    question: "In CSS,what is the correct option to select all the tags on a page?",
+    answer: "p{}",
+    options: [
+      "p{}",
+      "#p{}",
+      ".p{}",
+      "<p>{}"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Select the appropriate HTML tag used for the largest heading?",
+    answer: "H1",
+    options: [
+      "Heading",
+      "Head",
+      "H6",
+      "H1"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Select the appropriate HTML tag for inserting a line break?",
+    answer: "br",
+    options: [
+      "br",
+      "break",
+      "brbr",
+      "lb"
+    ]
+  },
+  
+];
